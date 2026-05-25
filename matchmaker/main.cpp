e5a24e60b2403397ba0052d9100d81cd195fa8f1
skurkaodogurka <kerudabp@gmail.com>
2026-05-25 18:39:18 +0200

    added bfs based coloring algorithm which checks if the graph is bipartite.

--- matchmaker/main.cpp
+++ matchmaker/main.cpp
@@ -8,6 +8,62 @@ struct edge {
 	long long w;	//waga polaczenia
 };
 
+struct queue {
+	int* elements;
+	int head = 0;
+	int tail = 0;
+	
+	void push(int value) {
+		elements[tail] = value;
+		tail++;
+	}
+	int pop() {
+		int value = elements[head];
+		head++;
+		return value;
+	}
+	bool isEmpty() {
+		if (head == tail) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+
+bool bfsEdgeColoring(int n, int* head, int* to, int* nextEdge, int* colors) {
+
+	queue q; //tworzenie lokalnej kolejki
+	q.elements = new int[n + 1];
+	for (int i = 1; i <= n; i++) {
+		if (colors[i] == -1) {
+			colors[i] = 0;
+			q.push(i);
+
+			while (!q.isEmpty()) {
+				int u = q.pop();
+				int currentEdge = head[u];
+				while (currentEdge != -1) {
+					int v = to[currentEdge];
+					if (colors[v] == -1) {
+						colors[v] = 1 - colors[u];
+						q.push(v);
+					}
+					else if (colors[v] == colors[u]) {
+						delete[] q.elements;
+						return false;
+					}
+					currentEdge = nextEdge[currentEdge];
+				}
+			}
+		}
+	}
+
+	delete[] q.elements;
+	return true;
+}
+
 void test() {
 	int n, m;
 	scanf("%d %d", &n, &m);
@@ -24,9 +80,11 @@ void test() {
 	int* to = new int[2 * m];
 	int* nextEdge = new int[2 * m];
 	long long* weight = new long long[2 * m];
+	int* colors = new int[n + 1];
 
 	for (int i = 0; i <= n; i++) {
 		head[i] = -1;
+		colors[i] = -1;
 	}
 
 	for (int i = 0; i < m; i++) {
@@ -49,11 +107,17 @@ void test() {
 		edgeCounter++;
 
 	}
+
+	bool isBipartite = bfsEdgeColoring(n, head, to, nextEdge, colors);
+
+	delete[] colors;
 	delete[] head;
 	delete[] weight;
 	delete[] nextEdge;
 	delete[] to;
 }
+
+
 
 int main() {
 	int t;
